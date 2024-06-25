e137bd1a5bb9e126b83efb7edd8579c14e17f21e
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2024-06-25 23:03:08 +0200

    Joining A Relay

--- Assets/Scripts/ClientGameManager.cs
+++ Assets/Scripts/ClientGameManager.cs
@@ -1,30 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// ClientGameManager manages client-side game operations, including initialization and scene management
 public class ClientGameManager
 {
-    private const string MenuSceneName = "Main Menu";
+    private JoinAllocation allocation; // Allocation for joining a relay server
 
+    private const string MenuSceneName = "Main Menu"; // Name of the main menu scene
+
+    // Method to initialize Unity services and authenticate the client asynchronously
     public async Task<bool> InitAsync()
     {
-        await UnityServices.InitializeAsync();
+        await UnityServices.InitializeAsync(); // Initialize Unity services
 
-        AuthState authState = await AuthenticationWrapper.DoAuth();
+        AuthState authState = await AuthenticationWrapper.DoAuth(); // Perform authentication
 
         if (authState == AuthState.Authenticated)
         {
-            return true;
+            return true; // Return true if authentication is successful
         }
 
-        return false;
+        return false; // Return false if authentication fails
     }
 
+    // Method to navigate to the main menu
     public void GoToMenu()
     {
-        SceneManager.LoadScene(MenuSceneName);
+        SceneManager.LoadScene(MenuSceneName); // Load the main menu scene
+    }
+
+    // Method to start the client asynchronously using a join code
+    public async Task StartClientAsync(string joinCode)
+    {
+        try
+        {
+            allocation = await Relay.Instance.JoinAllocationAsync(joinCode); // Join relay allocation using the join code
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e); // Log any exceptions
+            return;
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>(); // Get the UnityTransport component from the NetworkManager
+
+        RelayServerData relayServerData = new RelayServerData(allocation, "dtls"); // Create relay server data using the allocation
+        transport.SetRelayServerData(relayServerData); // Set the relay server data for the transport
+
+        NetworkManager.Singleton.StartClient(); // Start the client
     }
 }
